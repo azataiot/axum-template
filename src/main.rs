58b2d0d14c01bd7c0995849affe9b9d954637e64
Azat <8280770+azataiot@users.noreply.github.com>
2025-08-21 16:48:26 +0300

    Remove unused `Person` trait definition from `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 use std::collections::HashMap;
 
-fn main() {
-    trait Person {
-        fn get_name(&self) -> String;
-        fn get_age(&self) -> i32;
-        fn say(&self) {
-            println!("Hello, {}", self.get_name());
-        }
-    }
-}
+fn main() {}
